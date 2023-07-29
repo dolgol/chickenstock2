8f65bf617cb21fc394bbe16ccdb5405261f24ca9
kcs3378 <gcs3378@gmail.com>
2023-07-29 16:52:53 +0900

    fix file name test stocksDao -> StocksDao

---
+++ src/main/java/ssg/com/a/dao/stocksDao_lower.java
@@ -0,0 +1,18 @@
+package ssg.com.a.dao;
+
+import java.util.List;
+
+import ssg.com.a.dto.StockParam;
+import ssg.com.a.dto.StocksComment;
+import ssg.com.a.dto.StocksDto;
+
+public interface StocksDao {
+	
+	List<StocksDto> stockslist(StockParam param);
+	int getstocks(StockParam param);
+	StocksDto stocksdetail(String symbol);
+	
+	int stockscommentwrite(StocksComment comment); List<StocksComment>
+	stockscommentlist(String symbol);
+	
+}
